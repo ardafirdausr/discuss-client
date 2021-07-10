cfa341da826290569feadbc95e88c67087d55b39
Arda <ardafirdausr@gmail.com>
2021-07-10 11:15:45 +0700

    add login page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,11 +5,14 @@ import {
 } from "react-router-dom";
 
 import NotFound from './views/pages/NotFound';
+import Login from './views/pages/Login';
 
 const AppRouter = () => (
   <Router>
     <Switch>
-      <Route exact component={NotFound}/>
+      <Route exact path="/auth/login" component={Login}/>
+      <Route exact path="/" component={Login}/>
+      <Route component={NotFound}/>
     </Switch>
   </Router>
 )
